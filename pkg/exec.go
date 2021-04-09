628bd00c1ffdf2dafdf409524fa62f05b082d7b5
Purp1e <47248616+Purple-CSGO@users.noreply.github.com>
2021-04-10 01:23:08 +0800

    exec重写准备 TODO已理清

--- pkg/exec.go
+++ pkg/exec.go
@@ -1,8 +1,11 @@
 package ptools
 
 import (
+	"bufio"
 	"fmt"
+	"os/exec"
 	"strings"
+	"syscall"
 )
 
 //执行一次command指令 直接调用
@@ -11,6 +14,198 @@ func Exec(command string) (output string, err error) {
 
 	return ExecArgs(cmdArgs)
 }
+
+//// TODO:
+// @ 指令[]string的方法带args后缀 指令string的在分割指令之后调用args
+// 1. Exec 执行指令
+// 2. CMD 根据系统执行命令，调用Exec
+//    windows "cmd.exe", "/c", args...
+//    other   "/bin/bash", "-c", args...
+// 3. doRealtime 执行指令，实时对每行字符串进行操作
+//    - ExecRealtime
+//    - CMDRealtime
+// 4. doRealtimeControl 执行指令，实时对每行字符串进行操作，且实时暂停/继续/结束
+//    - ExecRealtimeControl
+//    - CMDRealtimeControl
+
+// 操作一：分割行
+// go func() {
+// 	scanner.Split(ScanCRandLF)
+// 	for scanner.Scan() {
+// 		对每一行的操作
+// 		method(string(scanner.Bytes()))
+// 	}
+// }()
+// 操作二：实时暂停/继续/结束 TODO: 解决实时控制的问题
+// 根据 win or others
+// realtimeControl
+
+func CMDRealtimeControlArgs(args []string, method func(line string), signal chan rune, winPssuspend string) error {
+	//cmd/bash传参是为了使用二者自带的命令，直接exec无法使用这些命令
+	args = append([]string{"-c"}, args...)
+	cmd := exec.Command("/bin/bash", args...)
+
+	//标准输出pipe
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return err
+	}
+
+	//标准错误pipe->标准输出pipe
+	cmd.Stderr = cmd.Stdout
+
+	//开始运行
+	if err = cmd.Start(); err != nil {
+		return err
+	}
+	scanner := bufio.NewScanner(stdout)
+
+	//实时处理输出
+	go func() {
+		scanner.Split(ScanCRandLF)
+		for scanner.Scan() {
+			//对每一行的操作
+			method(string(scanner.Bytes()))
+		}
+	}()
+
+	go func() {
+		for control := range signal {
+			switch control {
+			case 'p':
+				//暂停
+				cmd.Process.Signal(syscall.SIGTSTP) //win下不可用
+			case 'r':
+				//继续
+				cmd.Process.Signal(syscall.SIGCONT) //win下不可用
+			case 'q':
+				//中止
+				err = cmd.Process.Kill()
+				break
+			}
+		}
+	}()
+
+	return cmd.Wait()
+}
+
+//执行指令 实时控制 winPssuspend留空
+func ExecRealtimeControlArgs(args []string, method func(line string), signal chan rune, winPssuspend string) error {
+	//TODO
+	cmd := exec.Command(args[0], args[1:]...)
+
+	//标准输出pipe
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return err
+	}
+
+	//标准错误pipe->标准输出pipe
+	cmd.Stderr = cmd.Stdout
+
+	//开始运行
+	if err = cmd.Start(); err != nil {
+		return err
+	}
+	scanner := bufio.NewScanner(stdout)
+
+	//实时处理输出
+	go func() {
+		scanner.Split(ScanCRandLF)
+		for scanner.Scan() {
+			//对每一行的操作
+			method(string(scanner.Bytes()))
+		}
+	}()
+
+	go realtimeControl()
+
+	return cmd.Wait()
+}
+
+func doRealtimeControlArgs(args []string, method func(line string), signal chan<- rune, winPssuspend string) error {
+	//cmd/bash传参是为了使用二者自带的命令，直接exec无法使用这些命令
+	args = append([]string{"-c"}, args...)
+	cmd := exec.Command("/bin/bash", args...)
+
+	//标准输出pipe
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return err
+	}
+
+	//标准错误pipe->标准输出pipe
+	cmd.Stderr = cmd.Stdout
+
+	//开始运行
+	if err = cmd.Start(); err != nil {
+		return err
+	}
+	scanner := bufio.NewScanner(stdout)
+
+	//实时处理输出
+	go func() {
+		scanner.Split(ScanCRandLF)
+		for scanner.Scan() {
+			//对每一行的操作
+			method(string(scanner.Bytes()))
+		}
+	}()
+
+	go realtimeControl(cmd, signal)
+
+	return cmd.Wait()
+}
+
+//执行一次command指令
+func CMD(command string) (output string, err error) {
+	cmd := exec.Command("/bin/bash", "-c", command)
+
+	out, err := cmd.CombinedOutput()
+	return string(out), err
+}
+
+//参数以切片形式存放
+func ExecArgs(args []string) (output string, err error) {
+	cmd := exec.Command(args[0], args[1:]...)
+
+	out, err := cmd.CombinedOutput()
+	return string(out), err
+}
+
+//执行一次command指令且自定义方法处理每行结果
+func ExecRealtime(command string, method func(line string)) error {
+	//cmd/bash传参是为了使用二者自带的命令，直接exec无法使用这些命令
+	cmd := exec.Command("/bin/bash", "-c", command)
+
+	//标准输出pipe
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return err
+	}
+
+	//标准错误pipe->标准输出pipe
+	cmd.Stderr = cmd.Stdout
+
+	//开始运行
+	if err = cmd.Start(); err != nil {
+		return err
+	}
+	scanner := bufio.NewScanner(stdout)
+
+	//实时处理输出
+	go func() {
+		scanner.Split(ScanCRandLF)
+		for scanner.Scan() {
+			//对每一行的操作
+			method(string(scanner.Bytes()))
+		}
+	}()
+
+	return cmd.Wait()
+}
+
+////TODO: END
 
 //执行一次command指令且实时输出每行结果
 func ExecRealtimePrint(command string) error {
--- pkg/exec_win.go
+++ pkg/exec_win.go
@@ -3,8 +3,6 @@
 package ptools
 
 import (
-	"bufio"
-	"errors"
 	"fmt"
 	"log"
 	"os/exec"
@@ -13,61 +11,60 @@ import (
 	"syscall"
 )
 
-//执行一次command指令 经过cmd
-func CMD(command string) (output string, err error) {
-	cmd := exec.Command("cmd.exe", "/c", command)
+// //执行一次command指令 经过cmd
+// func CMD(command string) (output string, err error) {
+// 	cmd := exec.Command("cmd.exe", "/c", command)
 
-	//隐藏黑框 !仅win下用
-	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+// 	//隐藏黑框 !仅win下用
+// 	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
 
-	out, err := cmd.CombinedOutput()
-	return string(out), err
-}
+// 	out, err := cmd.CombinedOutput()
+// 	return string(out), err
+// }
 
-//参数以切片形式存放
-func ExecArgs(args []string) (output string, err error) {
-	cmd := exec.Command(args[0], args[1:]...)
+// //参数以切片形式存放
+// func ExecArgs(args []string) (output string, err error) {
+// 	cmd := exec.Command(args[0], args[1:]...)
 
+// 	out, err := cmd.CombinedOutput()
+// 	return string(out), err
+// }
 
-	out, err := cmd.CombinedOutput()
-	return string(out), err
-}
+// //执行一次command指令且自定义方法处理每行结果
+// func ExecRealtime(command string, method func(line string)) error {
+// 	//cmd/bash传参是为了使用二者自带的命令，直接exec无法使用这些命令
+// 	cmd := exec.Command("cmd.exe", "/c", command)
 
-//执行一次command指令且自定义方法处理每行结果
-func ExecRealtime(command string, method func(line string)) error {
-	//cmd/bash传参是为了使用二者自带的命令，直接exec无法使用这些命令
-	cmd := exec.Command("cmd.exe", "/c", command)
+// 	//隐藏黑框 !仅win下用
+// 	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
 
-	//隐藏黑框 !仅win下用
-	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+// 	//标准输出pipe
+// 	stdout, err := cmd.StdoutPipe()
+// 	if err != nil {
+// 		return err
+// 	}
 
-	//标准输出pipe
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		return err
-	}
+// 	//标准错误pipe->标准输出pipe
+// 	cmd.Stderr = cmd.Stdout
 
-	//标准错误pipe->标准输出pipe
-	cmd.Stderr = cmd.Stdout
+// 	//开始运行
+// 	if err = cmd.Start(); err != nil {
+// 		return err
+// 	}
+// 	scanner := bufio.NewScanner(stdout)
 
-	//开始运行
-	if err = cmd.Start(); err != nil {
-		return err
-	}
-	scanner := bufio.NewScanner(stdout)
+// 	//实时处理输出
+// 	go func() {
+// 		scanner.Split(ScanCRandLF)
+// 		for scanner.Scan() {
+// 			//对每一行的操作
+// 			method(string(scanner.Bytes()))
+// 		}
+// 		fmt.Println("debug...结束")
+// 	}()
 
-	//实时处理输出
-	go func() {
-		scanner.Split(ScanCRandLF)
-		for scanner.Scan() {
-			//对每一行的操作
-			method(string(scanner.Bytes()))
-		}
-		fmt.Println("debug...结束")
-	}()
-
-	return cmd.Wait()
-}
+// 	return cmd.Wait()
+// }
 
 //查找（环境变量+当前位置）可执行文件的位置
 func GetBinaryPath(binary string) (string, error) {
@@ -76,69 +73,99 @@ func GetBinaryPath(binary string) (string, error) {
 	return dir, err
 }
 
-//参数以切片形式存放
-func ExecRealtimeControlArgs(args []string, method func(line string), signal chan rune, winPssuspend string) error 	{
-	if exist := IsFileExisted(winPssuspend); !exist {
-		return errors.New("pssuspend.exe does not exist. check path string")
-	}
+// //参数以切片形式存放
+// func ExecRealtimeControlArgs(args []string, method func(line string), signal chan rune, winPssuspend string) error 	{
+// 	if exist := IsFileExisted(winPssuspend); !exist {
+// 		return errors.New("pssuspend.exe does not exist. check path string")
+// 	}
 
-	cmd := exec.Command(args[0], args[1:]...)
+// 	cmd := exec.Command(args[0], args[1:]...)
 
+// 	//隐藏黑框 !仅win下用
+// 	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+
+// 	//标准输出pipe
+// 	stdout, err := cmd.StdoutPipe()
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	//标准错误pipe->标准输出pipe
+// 	cmd.Stderr = cmd.Stdout
+
+// 	//开始运行
+// 	if err = cmd.Start(); err != nil {
+// 		return err
+// 	}
+// 	scanner := bufio.NewScanner(stdout)
+
+// 	//实时处理输出
+// 	go func() {
+// 		scanner.Split(ScanCRandLF)
+// 		for scanner.Scan() {
+// 			//对每一行的操作
+// 			method(string(scanner.Bytes()))
+// 		}
+// 	}()
+
+// 	go func() {
+// 		for control := range signal{
+// 			switch control {
+// 			case 'p':
+// 				//暂停
+// 				fmt.Println(FormatPath(winPssuspend) + " " + strconv.Itoa(cmd.Process.Pid))
+// 				if _, err := CMD(FormatPath(winPssuspend) + " " + strconv.Itoa(cmd.Process.Pid)); err != nil {
+// 					//log.Println(out)
+// 					log.Println(err)
+// 				}
+// 			case 'r':
+// 				//继续
+// 				fmt.Println(FormatPath(winPssuspend) + " -r " + strconv.Itoa(cmd.Process.Pid))
+// 				if _, err := CMD(FormatPath(winPssuspend) + " -r " + strconv.Itoa(cmd.Process.Pid)); err != nil {
+// 					//log.Println(out)
+// 					log.Println(err)
+// 				}
+// 			case 'q':
+// 				//中止
+// 				_ = cmd.Process.Kill()
+// 			}
+// 		}
+// 	}()
+
+// 	return cmd.Wait()
+// }
+
+func doHideWindow(cmd *exec.Cmd) {
 	//隐藏黑框 !仅win下用
 	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
-
-	//标准输出pipe
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		return err
-	}
-
-	//标准错误pipe->标准输出pipe
-	cmd.Stderr = cmd.Stdout
-
-	//开始运行
-	if err = cmd.Start(); err != nil {
-		return err
-	}
-	scanner := bufio.NewScanner(stdout)
-
-	//实时处理输出
-	go func() {
-		scanner.Split(ScanCRandLF)
-		for scanner.Scan() {
-			//对每一行的操作
-			method(string(scanner.Bytes()))
-		}
-	}()
-
-	go func() {
-		for control := range signal{
-			switch control {
-			case 'p':
-				//暂停
-				fmt.Println(FormatPath(winPssuspend) + " " + strconv.Itoa(cmd.Process.Pid))
-				if _, err := CMD(FormatPath(winPssuspend) + " " + strconv.Itoa(cmd.Process.Pid)); err != nil {
-					//log.Println(out)
-					log.Println(err)
-				}
-			case 'r':
-				//继续
-				fmt.Println(FormatPath(winPssuspend) + " -r " + strconv.Itoa(cmd.Process.Pid))
-				if _, err := CMD(FormatPath(winPssuspend) + " -r " + strconv.Itoa(cmd.Process.Pid)); err != nil {
-					//log.Println(out)
-					log.Println(err)
-				}
-			case 'q':
-				//中止
-				_ = cmd.Process.Kill()
-			}
-		}
-	}()
-
-	return cmd.Wait()
 }
 
-func doHideWindow(cmd *exec.Cmd)  {
-	//隐藏黑框 !仅win下用
-	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+// 	go func() {
+
+// 	}()
+
+func realtimeControl(cmd *exec.Cmd, signal chan rune) (err error) {
+	for control := range signal {
+		switch control {
+		case 'p':
+			//暂停
+			fmt.Println(FormatPath(winPssuspend) + " " + strconv.Itoa(cmd.Process.Pid))
+			if _, err := CMD(FormatPath(winPssuspend) + " " + strconv.Itoa(cmd.Process.Pid)); err != nil {
+				//log.Println(out)
+				log.Println(err)
+			}
+		case 'r':
+			//继续
+			fmt.Println(FormatPath(winPssuspend) + " -r " + strconv.Itoa(cmd.Process.Pid))
+			if _, err := CMD(FormatPath(winPssuspend) + " -r " + strconv.Itoa(cmd.Process.Pid)); err != nil {
+				//log.Println(out)
+				log.Println(err)
+			}
+		case 'q':
+			//中止
+			_ = cmd.Process.Kill()
+		}
+	}
+
+	return
 }
